52de1d79bc300f3fa4ddc63af5afee7daaf21378
Sxyndicate <99019754+Sxyndicate@users.noreply.github.com>
2022-07-22 14:47:41 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,114 @@ const showMenu = (toggleId, navId) =>{
     }
 }
 showMenu('nav-toggle','nav-menu')
+
+/* -- Remove Menu mobile -- */
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* -- Scroll sections Active link -- */
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/* --- Show Scroll Top --- */
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+/* --- Dark Light Theme --- */
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/* --- Reduce The Size & Print oOn A4 Sheet --- */
+function scaleCv(){
+    document.body.classList.add('scale-cv')
+}
+
+/* --- Remove the size when CV is downloaded   --- */
+function removeScale(){
+    document.body.classList.remove('scale-cv')
+}
+
+/* --- Generate PDF    --- */
+//PDF generated area
+let areaCV = document.getElementById('area-cv')
+
+let resumeButton = document.getElementById('resume-button')
+
+
+//Html2pdf options
+let opt = {
+    margin:       0,
+    filename:     'myResume.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 4 },
+    jsPDF:        { format: 'a4', orientation: 'portrait' }
+}
+
+//Function to call areaCV and html2PDF options
+function generateResume(){
+    html2pdf(areaCV, opt)
+}
+
+//When the button is clicked, it executes three functions
+resumeButton.addEventListener('click', () =>{
+    //1. The class scale-cv is added to the body where it reduces the size
+    scaleCv()
+
+    //2. The PDF is generated
+    generateResume
+    
+    //3. The .scale-cv class is removed from the body after 5 seconds 
+    setTimeout(removeScale, 5000)
+})
